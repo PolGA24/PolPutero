fc94209a9c5de40ba0b6e05bb932f838403aa345
Gerard Fornés <gerard.fornes@gracia.lasalle.cat>
2025-04-03 11:01:57 +0200

    ejercicio 1 extras triggers

--- BBDD/Exercicis extras Triggers.sql
+++ BBDD/Exercicis extras Triggers.sql
@@ -6,6 +6,37 @@ de nom sobre la taula movies, la taula movies_version tindrà tres files per a l
 Pensa com resoldràs la situació de saber quin número de versió estem.
 */
 
+CREATE TABLE IF NOT EXISTS movies_version (
+    id int(11) NOT NULL,
+    name varchar(100) DEFAULT NULL,
+    year int(11) DEFAULT NULL,
+    stockUnits int(11) DEFAULT NULL,
+    price double DEFAULT NULL,
+    hora_update datetime DEFAULT NULL,
+    version int(11) NOT NULL,
+    PRIMARY KEY (id, version)
+);
+
+DELIMITER $$
+DROP TRIGGER IF EXISTS before_update_movies $$
+CREATE TRIGGER before_update_movies
+BEFORE UPDATE ON movies
+FOR EACH ROW
+BEGIN
+
+    SET @vversion = (SELECT COUNT(*) FROM movies_version WHERE id = OLD.id) + 1;
+    INSERT INTO movies_version (id, name, year, stockUnits, price, hora_update, version)
+    VALUES (OLD.id, OLD.name, OLD.year, OLD.stockUnits, OLD.price, NOW(), @vversion);
+END $$
+DELIMITER ;
+
+
+/*PROVAREM*/
+UPDATE movies SET name = 'El Padrino' WHERE id = 1;
+SELECT * FROM movies_version WHERE id = 1 ORDER BY version DESC;
+
+
+
 /*
 Crea un altre trigger per a la taula movies de la mateixa base de dades anterior. Aquest cop volem que s'executi després d'un INSERT. Dins del
 cos del trigger, volem que guardi una fila dins d'una altra taula nova (que haurem de crear). Aquesta taula nova que hem de crear amb el nom de
