ae49d1d27cbd3c39798f5ee27f4f9db203d16dc7
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2025-04-04 09:49:34 +0200

    Ejercicio 2 mision 2 jedi

---
+++ Java/Ejercicios Jedi/Mision 2/Ejercicio2/src/Main.java
@@ -0,0 +1,11 @@
+/*
+2. L'Editor d'Imatges RAW del Mestre Pixel 🎨
+Enunciat: Desenvolupa un programa que llegeixi fitxers d'imatge RAW i realitzi operacions com invertir colors o aplicar filtres.
+Repte Épic: Desa el resultat en un nou fitxer binari.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
