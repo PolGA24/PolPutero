30afd52d27b032bb6f7a845e856211ebfa0b86ec
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2025-04-04 10:05:59 +0200

    Creacion clase crear personaje

--- Java/Ejercicios Jedi/Mision 2/Ejercicio1/src/Main.java
+++ Java/Ejercicios Jedi/Mision 2/Ejercicio1/src/Main.java
@@ -4,8 +4,49 @@ Enunciat: Defineix una classe Java amb atributs i crea un programa que convertei
 Repte Extra: Desxifra el fitxer binari amb un altre programa i mostra els atributs dels objectes.
  */
 
+import java.io.DataOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Personaje> personajes = new ArrayList();
+        boolean salir = false;
+
+        while (!salir) {
+            int op;
+            System.out.println("SELECCIONE OPCION");
+            System.out.println("1. Agregar personaje");
+            System.out.println("2. Salir");
+            System.out.printf(">");
+            op = sc.nextInt();
+
+            switch (op) {
+                case 1:
+                    personajes.add(crearPersonaje());
+                    break;
+            }
+        }
+    }
+    public static Personaje crearPersonaje() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printf("Ingrese nombre: ");
+        String nombre = sc.nextLine();
+        System.out.printf("Ingrese edad: ");
+        int edad = sc.nextInt();
+        System.out.printf("Ingrese email: ");
+        String email = sc.nextLine();
+
+        Personaje personaje = new Personaje(nombre, edad, email);
+        return personaje;
+    }
+    public static void write(Personaje personaje) throws FileNotFoundException {
+        FileOutputStream fileOut = new FileOutputStream("personaje.data");
+        DataOutputStream output = new DataOutputStream(fileOut);
+
     }
 }
