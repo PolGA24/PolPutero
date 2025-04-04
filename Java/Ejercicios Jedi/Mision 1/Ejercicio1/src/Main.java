23e182f25f11a3df9cb1262ce16b23fda36ea749
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2025-04-04 09:43:15 +0200

    Enunciado ejercicio1 mision 1

--- Java/Ejercicios Jedi/Mision 1/Ejercicio1/src/Main.java
+++ Java/Ejercicios Jedi/Mision 1/Ejercicio1/src/Main.java
@@ -1,3 +1,9 @@
+/*
+1. El Comptador de Paraules Màgic 🧙‍♂️
+Enunciat: Crea un programa que llegeixi un fitxer de text i reveli el nombre total de paraules que hi habiten.
+Repte Extra: Transforma't en un mag de les paraules! Compta la freqüència de cada paraula i ordena-les de més a menys freqüents.
+ */
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
