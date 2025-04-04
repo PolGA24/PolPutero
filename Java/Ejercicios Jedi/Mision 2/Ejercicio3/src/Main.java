d610afab8a5a377355d0a4c296f086d502470913
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2025-04-04 09:50:15 +0200

    Ejercicio 3 mision 2 jedi

---
+++ Java/Ejercicios Jedi/Mision 2/Ejercicio3/src/Main.java
@@ -0,0 +1,11 @@
+/*
+3. El Gestor de Registres Secret 📦
+Enunciat: Crea un programa que converteixi un fitxer binari en una base de dades simple.
+Poder Especial: Permet inserir, buscar, modificar i eliminar registres.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
