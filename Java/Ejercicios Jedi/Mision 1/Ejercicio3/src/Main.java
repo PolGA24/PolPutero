f0d016f9726284bb77e231293d0f4bfa06324524
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2025-04-04 09:45:56 +0200

    Ejercicio 3 mision 1 jedi

---
+++ Java/Ejercicios Jedi/Mision 1/Ejercicio3/src/Main.java
@@ -0,0 +1,11 @@
+/*
+3. L'Editor de Text del Futur ✍️
+Enunciat: Construeix un editor de text que permeti crear, obrir, editar i desar fitxers de text.
+Repte Épic: Afegeix poders especials com buscar i reemplaçar text!
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
