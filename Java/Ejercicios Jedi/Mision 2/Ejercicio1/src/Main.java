14c2a4ac5e5a4707e2c237595d7f23e38c398df2
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2025-04-04 09:48:53 +0200

    ejercicio 1 mision 2 jedi

---
+++ Java/Ejercicios Jedi/Mision 2/Ejercicio1/src/Main.java
@@ -0,0 +1,11 @@
+/*
+1. La Serialització d'Objectes Màgica 🔮
+Enunciat: Defineix una classe Java amb atributs i crea un programa que converteixi objectes d'aquesta classe en un fitxer binari.
+Repte Extra: Desxifra el fitxer binari amb un altre programa i mostra els atributs dels objectes.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
