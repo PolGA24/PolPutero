d0343ad634651f63d8393bc1da413d4456445e14
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2025-04-04 09:55:45 +0200

    Creacion clase personaje

---
+++ Java/Ejercicios Jedi/Mision 2/Ejercicio1/src/Personaje.java
@@ -0,0 +1,11 @@
+public class Personaje {
+    private String nombre;
+    private int edad;
+    private String correo;
+
+    public Personaje(String nombre, int edad, String correo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.correo = correo;
+    }
+}
