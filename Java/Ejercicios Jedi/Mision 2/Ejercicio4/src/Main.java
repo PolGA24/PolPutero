0f8b95576800706cf7c4029284a4d609e93b7a30
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2025-04-04 09:51:42 +0200

    Ejercicio 4 mision 2 jedi

---
+++ Java/Ejercicios Jedi/Mision 2/Ejercicio4/src/Main.java
@@ -0,0 +1,11 @@
+/*
+4. El Desxifrador d'Estructures de Dades Complexes 🧩
+Enunciat: Crea un programa que llegeixi un fitxer binari que emmagatzemi dades com arbres o grafs.
+Repte Épic: Mostra les dades en un format que els humans puguin entendre.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
