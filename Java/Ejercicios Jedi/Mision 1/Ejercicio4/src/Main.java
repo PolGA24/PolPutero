c91af890ca275514c319f293b1c1601026eea80e
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2025-04-04 09:46:50 +0200

    Ejercicio 4 mision 1

---
+++ Java/Ejercicios Jedi/Mision 1/Ejercicio4/src/Main.java
@@ -0,0 +1,11 @@
+/*
+4. El Generador d'Informes Visionari 📊
+Enunciat: Crea un programa que llegeixi dades d'un fitxer CSV (com una taula màgica) i generi un informe en format text o HTML.
+Exemple: Transforma dades de vendes en un informe que mostri el total de vendes per producte.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
