4c48dff69f449818430fc62d0d257f92d3203a36
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2025-04-04 09:44:31 +0200

    Ejercicio 2 mision 1

---
+++ Java/Ejercicios Jedi/Mision 1/Ejercicio2/src/Main.java
@@ -0,0 +1,10 @@
+/*
+2. El Filtre de Línies Selector 🕵️‍♀️
+Enunciat: Desenvolupa un programa que llegeixi un fitxer de text línia per línia i seleccioni només les línies que compleixin una condició secreta (per exemple, contenir una paraula clau). Escriu les línies seleccionades en un nou fitxer.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
