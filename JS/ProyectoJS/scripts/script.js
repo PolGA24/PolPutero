78b16f2afeca707e78873a266883fb9163af3d0e
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2025-04-06 18:51:58 +0200

    Sistema de cambio a modo oscuro con implementacion de localStorage

--- JS/ProyectoJS/scripts/script.js
+++ JS/ProyectoJS/scripts/script.js
@@ -11,16 +11,29 @@ window.onload = function() {
         document.getElementById("cookieModal").style.display = "block";
     }
 };
+window.addEventListener('DOMContentLoaded', () => {
+    const modoGuardado = localStorage.getItem('modo');
+    const boton = document.getElementById("modoBoton");
+    if (modoGuardado === 'oscuro') {
+        document.body.classList.add("oscuro");
+        boton.textContent = "☀️";
+    } else {
+        document.body.classList.remove("oscuro");
+        boton.textContent = "🌙";
+    }
+});
 function aceptarCookies() {
     localStorage.setItem("cookiesAceptadas", true);
     document.getElementById("cookieModal").style.display = "none";
 }
-function cambiarModo() {
+function cambiarModoOscuro() {
     document.body.classList.toggle("oscuro");
     const boton = document.getElementById("modoBoton");
     if (document.body.classList.contains("oscuro")) {
         boton.textContent = "☀️";
+        localStorage.setItem('modo', 'oscuro');
     } else {
         boton.textContent = "🌙";
+        localStorage.setItem('modo', 'claro');
     }
 }
